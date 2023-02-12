b5154b52cd25a47c71c90bef45430d25699362a4
Alexander Kudryashov <sasha080193@gmail.com>
2023-02-12 23:37:08 +0600

    Добавлены Лейбл счетчика, кнопка рестарта, кнопка счетчика в Сториборд Настроены в сториборде констрейны и вертикальный стек вью Связаны (отлёты и Экщн) элементы с Вьюконтроллером

--- ClickCounterAK/ViewController.swift
+++ ClickCounterAK/ViewController.swift
@@ -8,12 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var labelCounter: UILabel!
+    @IBOutlet weak var buttonRestartOutlet: UIButton!
+    @IBOutlet weak var buttonClickOutlet: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func buttonRestartAction(_ sender: Any) {
+    }
+    
+    
+    @IBAction func buttonClickAction(_ sender: Any) {
+    }
+    
 }
 
