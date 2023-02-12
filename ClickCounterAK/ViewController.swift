7f89daff10ddd256a45c35215fb02ea7c3975f1c
Alexander Kudryashov <sasha080193@gmail.com>
2023-02-12 22:44:15 +0600

    Initial Commit

---
+++ ClickCounterAK/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ClickCounterAK
+//
+//  Created by Александр Кудряшов on 12.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
